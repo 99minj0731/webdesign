834eba83dad4b9216d22ab083d2ee5c3bce823ee
99minj0731 <99minj0731@naver.com>
2025-03-02 00:06:17 +0900

    A-1 JQuery

--- A/A_1/js/script.js
+++ A/A_1/js/script.js
@@ -1,4 +1,63 @@
 $(function () {
   //문서를 로딩 후 실행하라
-  // alert("경고");
+
+  //슬라이드
+
+  function slide() {
+    $(".slide ul").animate({ top: "-300px" }, 1000, function () {
+      $(".slide ul").append($(".slide ul li").first());
+      $(".slide ul").css({ top: "0" });
+    });
+  }
+
+  setInterval(slide, 3000);
+
+  //탭메뉴
+  $(".tabmenu>li").click(function () {
+    $(".tabmenu>li").removeClass("on");
+    $(this).addClass("on");
+
+    let i = $(this).index();
+
+    // console.log(i);
+
+    $(".tabcon").hide();
+    $(".tabcon").eq(i).show();
+
+    return false; //링크차단용
+  });
+
+  //메뉴
+  $("nav>ul>li").mouseenter(function () {
+    $(".sub").stop().slideDown();
+  });
+
+  $("nav>ul>li").mouseleave(function () {
+    $(".sub").stop().slideUp();
+  });
+
+  //키보드이벤트 메뉴
+  $("nav>ul>li").focusin(function () {
+    $(".sub").slideDown();
+    // $("nav>ul>li").removeClass("on");
+    $(this).addClass("on");
+  });
+
+  $(".sub li:last-child").focusout(function () {
+    $("nav>ul>li").removeClass("on");
+  });
+
+  $("nav li:last-child li:last-child").focusout(function () {
+    $(".sub").slideUp();
+  });
+
+  // 팝업
+  $(".pp").click(function () {
+    $(".popup").show();
+    return false; //링크차단
+  });
+
+  $("button").click(function () {
+    $(".popup").hide();
+  });
 });
