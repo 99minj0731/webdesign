df7488bce68802bdd4599db783a6116196bbc9f4
99minj0731 <99minj0731@naver.com>
2025-03-02 14:06:11 +0900

    A-2 JQeury

--- A/A_2/js/script.js
+++ A/A_2/js/script.js
@@ -1,3 +1,39 @@
 $(function(){
+  //메뉴 키보드 이벤트
+  $("nav>ul>li>a").focusin(function(){
+    $(this).parent("li").addClass("on")
+    $(".sub, .menuBg").slideDown()
+  })
 
+  $("nav li li:last-child").focusout(function(){
+    $("nav li").removeClass("on")
+  })
+
+  $("nav li:last-child li:last-child").focusout(function(){
+    $(".sub, .menuBg").slideUp()
+  })
+  //슬라이드
+  function slide(){
+    $(".slide ul").animate({left:"-1200px"},2000,function(){
+      $(".slide ul").append($(".slide ul li").first())
+      $(".slide ul").css({left:0})
+    })
+  }
+  setInterval(slide,2000)
+
+  //메뉴
+  $("nav>ul>li").mouseenter(function(){
+    $(".sub, .menuBg").stop().slideDown()
+  })
+  $("nav>ul>li").mouseleave(function(){
+    $(".sub, .menuBg").stop().slideUp()
+  })
+  //팝업
+  $(".pp").click(function(){
+    $(".popup").show()
+  })
+
+  $(".popup button").click(function(){
+    $(".popup").hide()
+  })
 })
