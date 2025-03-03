12fd0e4837adffcddf0f5129eafab7d4322e42fb
99minj0731 <99minj0731@naver.com>
2025-03-03 16:20:37 +0900

    B-1 JQuery

--- B/B_1/js/script.js
+++ B/B_1/js/script.js
@@ -1,1 +1,55 @@
-$(function() {})
+$(function() {
+  //키보드 웹접근성
+  $("nav>ul>li").focusin(function(){
+    $(".sub").slideDown()
+    $(this).addClass("on")
+  })
+
+  $(".sub>li:last-child").focusout(function(){
+    $("nav>ul>li").removeClass("on")
+  })
+
+  $("nav li:last-child li:last-child").focusout(function(){
+    $(".sub").slideUp()
+  })
+
+  //탭메뉴
+
+  let i = 0
+
+  $(".tabmenu>li").click(function(){
+    $(".tabmenu>li").removeClass("on")
+    $(this).addClass("on")
+
+    i = $(this).index()
+    
+    $(".tabcon").hide()
+    $(".tabcon").eq(i).show()
+    return false
+  })
+
+  //슬라이드
+  function slide(){
+    $(".slide ul").animate({left:"-1200px"},1500,function(){
+      $(".slide ul").append($(".slide ul li").first())
+      $(".slide ul").css({left:0})
+    })
+  }
+  setInterval(slide,2000)
+  //메뉴
+  $("nav>ul>li").mouseenter(function(){
+    $(".sub").stop().slideDown()
+  })
+  $("nav>ul>li").mouseleave(function(){
+    $(".sub").stop().slideUp()
+  })
+
+  // 팝업
+  $(".pp").click(function(){
+    $(".popup").show()
+    return false
+  })
+  $(".button").click(function(){
+    $(".popup").hide()
+  })
+})
