cab8986a797da0d7e922b9a63b80d7de4e116ffd
99minj0731 <99minj0731@naver.com>
2025-03-05 22:51:04 +0900

    D-1 JQeury

--- D/D_1/js/script.js
+++ D/D_1/js/script.js
@@ -1,1 +1,51 @@
-$(function() {})
+$(function() {
+  //키보드 이벤트
+
+  $("nav>ul>li").focusin(function() {
+    $(this).find(".sub").slideDown()
+    $(this).addClass("on")
+  })
+
+  $(".sub li:last-child").focusout(function() {
+    $(".sub").slideUp()
+    $("nav>ul>li").removeClass("on")
+  })
+  //슬라이드
+
+  function slide(){
+    $(".slide ul").animate({top:"-400px"},1000,function(){
+      $(".slide ul").append($(".slide ul li").first())
+      $(".slide ul").css({top:0})
+    })
+  }
+
+  setInterval(slide,2000)
+  //nav
+  $("nav>ul>li").mouseenter(function(){
+    $(this).find(".sub").stop().slideDown()
+  })
+  $("nav>ul>li").mouseleave(function(){
+    $(this).find(".sub").stop().slideUp()
+  })
+
+  //탭메뉴
+  $(".tabmenu>li").click(function() {
+    $(".tabmenu>li").removeClass("on")
+    $(this).addClass("on")
+
+    let i = $(this).index()
+
+    $(".tabcon").hide()
+    $(".tabcon").eq(i).show()
+    return false
+  })
+
+  //팝업
+  $(".pp").click(function() {
+    $(".popcon").show();
+    return false;
+  })
+  $("button").click(function() {
+    $(".popcon").hide();
+  })
+})
