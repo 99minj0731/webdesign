6320b3e68c382684d612b7c6040b015327e51c76
99minj0731 <99minj0731@naver.com>
2025-03-15 23:31:38 +0900

    D-2 Jquery 진행중

--- D/D_2/js/script.js
+++ D/D_2/js/script.js
@@ -1,3 +1,20 @@
 $(function(){
-  
+  //슬라이드
+
+  function slide(){
+    $(".slide ul").animate({marginLeft:"-100%"},2000,function(){
+      $(".slide ul").append($(".slide ul li").first())
+      $(".slide ul").css({marginLeft:0})
+    })
+  }
+
+  setInterval(slide,3000)
+  //팝업
+  $(".pp").click(function(){
+    $(".popup").show()
+    return false
+  })
+  $(".button").click(function(){
+    $(".popup").hide()
+  })
 })
