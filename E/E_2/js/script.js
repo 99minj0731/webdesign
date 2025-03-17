0b4e7f67c5ca393f30bd48b206983b99971222bc
99minj0731 <99minj0731@naver.com>
2025-03-17 22:05:10 +0900

    E-2 Jquery

--- E/E_2/js/script.js
+++ E/E_2/js/script.js
@@ -1,3 +1,41 @@
 $(function(){
-  
+  //키보드 이벤트
+  $("nav>ul>li").focusin(function(){
+    $(this).addClass("on")
+    $(this).find(".sub").slideDown()
+  })
+  $(".sub>li:last-child").focusout(function(){
+    $("nav ul li").removeClass("on")
+    $(".sub").slideUp()
+  })
+
+  //슬라이드
+  function slide() {
+    $(".slide ul").animate({top: "-700px"},2000,function(){
+      $(".slide ul").append($(".slide ul li").first())
+      $(".slide ul").css({top:0})
+    })
+  }
+
+  setInterval(slide, 3000)
+
+  //네브바
+  $("nav>ul>li").mouseenter(function(){
+    $(this).addClass("on")
+    $(this).find($(".sub")).stop().slideDown()
+  })
+  $("nav>ul>li").mouseleave(function(){
+    $(this).removeClass("on")
+    $(this).find($(".sub")).stop().slideUp()
+  })
+
+  //팝업
+  $(".pp").click(function(){
+    $(".popup").show()
+    return false
+  })
+
+  $("button").click(function(){
+    $(".popup").hide()
+  })
 })
