2023f7e7babf0c117b4f49eee810101b9653c5ca
99minj0731 <99minj0731@naver.com>
2025-03-19 00:37:52 +0900

    F-2 JQUERY

--- F/F_2/js/script.js
+++ F/F_2/js/script.js
@@ -1,3 +1,38 @@
 $(function(){
+  //슬라이드
   
+  let i = 0
+  
+  function slide(){
+    if(i<2){
+      i++
+    }
+    else {
+      i = 0
+    }
+    $(".slide>ul>li").fadeOut(2000)
+    $(".slide>ul>li").eq(i).fadeIn(2000)
+  }
+
+  setInterval(slide,3000)
+  
+
+
+  //네비게이션 바
+  $("nav>ul>li").mouseenter(function(){
+    $(this).children(".sub").stop().slideDown()
+  })
+  $("nav>ul>li").mouseleave(function(){
+    $(this).find(".sub").stop().slideUp()
+  })
+
+  //팝업
+  $(".pp").click(function(){
+    $(".popup").show()
+    return false
+  })
+
+  $("button").click(function(){
+    $(".popup").hide()  
+  })
 })
