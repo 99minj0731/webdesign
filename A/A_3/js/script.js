7f218169e4477259a0a0b14918615c13fdd258ce
99minj0731 <99minj0731@naver.com>
2025-03-19 22:30:56 +0900

    A-3 JQUERY

--- A/A_3/js/script.js
+++ A/A_3/js/script.js
@@ -1,3 +1,48 @@
 $(function(){
+  //탭 콘
+  $(".tabmenu>li").click(function(){
+    $(".tabmenu li").removeClass("on")
+    $(".tabcon").hide()
+    $(this).addClass("on")
+    $(this).find(".tabcon").show()
+
+  })
+
+  // 슬라이드
   
+  let i = 0
+
+  function slide(){
+    if(i<2){
+      i++
+    }
+    else{
+      i=0
+    }
+  
+    $(".slide>ul>li").fadeOut(2000)
+    $(".slide>ul>li").eq(i).fadeIn(2000)
+  }
+
+  setInterval(slide,2000)
+
+  //네비게이션 바
+  $("nav>ul>li").mouseenter(function(){
+    $(this).find(".sub").stop().slideDown()
+    $(this).addClass("on")
+  })
+  $("nav>ul>li").mouseleave(function(){
+    $(this).find(".sub").stop().slideUp()
+    $(this).removeClass("on")
+  })
+  
+
+  //팝업
+  $(".pp").click(function(){
+    $(".popup").show()
+    return false
+  })
+  $("button").click(function(){
+    $(".popup").hide()
+  })
 })
