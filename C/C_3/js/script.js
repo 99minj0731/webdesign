78a9531d59ae0ec64ffaf322d66b84f569ec8e66
99minj0731 <99minj0731@naver.com>
2025-03-21 23:06:42 +0900

    C-3 Jquery

--- C/C_3/js/script.js
+++ C/C_3/js/script.js
@@ -1,3 +1,42 @@
 $(function(){
-  
+  //키보드 
+  $("nav>ul>li").focusin(function(){
+    $(this).find(".sub").stop().slideDown()
+    $(this).addClass("on")
+  })
+
+  $(".sub>li:last-child").focusout(function(){
+    $(".sub").stop().slideUp()
+    $("nav>ul>li").removeClass("on")
+  })
+  // 팝업
+  $(".pp").click(function(){
+    $(".popup").show()
+    return false
+  })
+  $("button").click(function(){
+    $(".popup").hide()
+    return false
+  })
+  //슬라이드
+
+  function slide() {
+    $(".slide ul").animate({left:"-800px"},2000,function(){
+      $(".slide ul").append($(".slide>ul>li").first())
+      $(".slide ul").css({left:0})
+    })
+  }
+
+  setInterval(slide,3000)
+
+
+  // 메뉴
+  $("nav>ul>li").mouseenter(function(){
+    $(this).find(".sub").stop().slideDown()
+    $(this).addClass("on")
+  })
+  $("nav>ul>li").mouseleave(function(){
+    $(this).find(".sub").stop().slideUp()
+    $(this).removeClass("on")
+  })
 })
