e60f5e0a434a55a0d670668ec59b2e2a66121ca2
99minj0731 <99minj0731@naver.com>
2025-03-22 17:59:23 +0900

    D-3 JQuery

--- D/D_3/js/script.js
+++ D/D_3/js/script.js
@@ -1,3 +1,53 @@
 $(function() {
-  
+  //탭 메뉴
+  $(".con li").click(function(){
+    $(".con li").removeClass("on")
+    $(this).addClass("on")
+    $(".tabcon").hide()
+    $(this).find(".tabcon").show(function(){
+      $(this).css({display:"flex"})
+    })
+  })
+
+
+  //팝업 
+  $(".pp").click(function(){
+    $(".popup").show()
+    return false
+  })
+  $("button").click(function(){
+    $(".popup").hide()
+  })
+  //슬라이드
+  $(".slide ul li").hide()
+  $(".slide ul li").eq(0).show()
+
+  let i = 0
+  function slide (){
+    if(i<2){
+      i++
+    }
+    else{
+      i=0
+    }
+    $(".slide ul li").fadeOut(2000)
+    $(".slide ul li").eq(i).fadeIn(2000)
+  }
+
+  setInterval(slide, 2000)
+
+
+  //네브바
+  $("nav>ul>li>h2").mouseenter(function(){
+    $(".sub").stop().slideDown(function(){
+      $(".sub").css({display:"flex"})
+    })
+    $(this).parent().addClass("on")
+  })
+
+
+  $("nav>ul>li").mouseleave(function(){
+    $("nav>ul>li").removeClass("on")
+    $(".sub").stop().slideUp()
+  })
 })
