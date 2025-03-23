0b8aed72de6fef26f5f82b6ce847675bd39ecd12
99minj0731 <99minj0731@naver.com>
2025-03-23 11:36:22 +0900

    E-4 Jquery

--- E/E_4/js/script.js
+++ E/E_4/js/script.js
@@ -1,6 +1,31 @@
 $(function(){
+  //팝업
+  $(".pp").click(function(){
+    $(".popup").show()  
+    return false
+  })
+  $("button").click(function(){
+    $(".popup").hide()  
+  })
+
+  //슬라이드 
+  let move = $(".slides").width()
+
+  function slide(){
+    $(".slide>ul").animate({left:-(move)}, 3000, function(){
+      $(".slide>ul").append($(".slide>ul>li").first())
+      $(".slide>ul").css({left:0})
+    })
+  }
+
+  setInterval(slide, 2000)
+
+
   // 네브바
   $("nav>ul>li").mouseenter(function(){
-    $(this).find(".sub").slidedown()
+    $(this).find(".sub").stop().slideDown()
+  })
+  $("nav>ul>li").mouseleave(function(){
+    $(this).find(".sub").stop().slideUp()
   })
 })
